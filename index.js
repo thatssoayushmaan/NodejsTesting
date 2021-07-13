8bfc1842656f0ba2beb1632c64f2b45160cadf94
Ayushmaan Verma <56289697+thatssoayushmaan@users.noreply.github.com>
2021-07-13 17:57:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
 const mongo = require('./shared/mongo')
-
+const studentRoute = require('./routes/student')
 
 const app = express()
-mongo.connect()
 
-const port = 3001
-app.listen(port, () => {
-    console.log(`Server up and running at ${port}`)
+
+async function loadApp(){
+    try {
+        await mongo.connect()
+        //Middelware for routes
+
+        app.use(cors())
+        app.use('/students', studentRoute)
+        //Parse your request body into json format
+        app.use(express.json())
+
+
+        const port = process.env.PORT || 3001
+        app.listen(port, () => {
+        console.log(`Server up and running at ${port}`)
 })
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+loadApp()
